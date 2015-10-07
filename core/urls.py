33df25e798aab82f61e9efdb98102c3c27cf577a
Will <wtprattstephen@quinnipiac.edu>
2015-10-07 05:12:19 +0000

    Add user profiles

--- core/urls.py
+++ core/urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
      url(r'^question/(?P<question_pk>\d+)/answer/update/(?P<answer_pk>\d+)/$', login_required(AnswerUpdateView.as_view()), name='answer_update'),
      url(r'^question/(?P<question_pk>\d+)/answer/delete/(?P<answer_pk>\d+)/$', login_required(AnswerDeleteView.as_view()), name='answer_delete'),
      url(r'^vote/$', login_required(VoteFormView.as_view()), name='vote'),
+     url(r'^user/(?P<slug>\w+)/$', login_required(UserDetailView.as_view()), name='user_detail'),
+         
 )
--- core/views.py
+++ core/views.py
@@ -134,3 +134,9 @@ class VoteFormView(FormView):
       else:
           prev_votes[0].delete()
       return redirect('question_list')
+    
+class UserDetailView(DetailView):
+      model = User
+      slug_field = 'username'
+      template_name = 'user/user_detail.html'
+      context_object_name = 'user_in_view'
