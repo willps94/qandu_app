b133bbac3fd44b87054b95226a02955667e6ab2e
Will <wtprattstephen@quinnipiac.edu>
2015-10-07 05:25:47 +0000

    Add questions and answers to user profiles

--- core/views.py
+++ core/views.py
@@ -140,3 +140,12 @@ class UserDetailView(DetailView):
       slug_field = 'username'
       template_name = 'user/user_detail.html'
       context_object_name = 'user_in_view'
+      
+      def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        user_in_view = User.objects.get(username=self.kwargs['slug'])
+        questions = Question.objects.filter(user=user_in_view)
+        context['questions'] = questions
+        answers = Answer.objects.filter(user=user_in_view)
+        context['answers'] = answers
+        return context
